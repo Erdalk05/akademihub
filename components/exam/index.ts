eccca7260b5487b8530c3dc270018c3ed7163a1b
Erdal Kiziroğlu <mailin@ornek.com>
2026-01-14 10:17:26 +0300

    cleanup: remove all non-spectra legacy exam code

--- components/exam/index.ts
+++
@@ -1,7 +0,0 @@
-// ============================================================================
-// EXAM COMPONENTS - Export Index
-// ============================================================================
-
-export { ExamTable } from './ExamTable';
-export { ExamRow } from './ExamRow';
-export { ExamRowDetails } from './ExamRowDetails';
