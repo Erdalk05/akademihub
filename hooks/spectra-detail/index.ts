3ff35a7da25a22c3b1a585e95c06dea4a52e5425
Erdal Kiziroğlu <mailin@ornek.com>
2026-01-16 17:35:45 +0300

    chore: remove legacy spectra code, keep wizard-only flow

--- hooks/spectra-detail/index.ts
+++
@@ -1,9 +0,0 @@
-// ============================================================================
-// SPECTRA DETAIL HOOKS - INDEX
-// ============================================================================
-
-export { useSpectraDetail } from './useSpectraDetail';
-export { useStudentFilters } from './useStudentFilters';
-export { useSpectraExport } from './useSpectraExport';
-export { useOrganizationTrend } from './useOrganizationTrend';
-
--- lib/spectra-detail/index.ts
+++
@@ -1,9 +0,0 @@
-// ============================================================================
-// SPECTRA DETAIL LIB - INDEX
-// ============================================================================
-
-export * from './constants';
-export * from './calculations';
-export { exportToExcel } from './export-excel';
-export { exportToPDF, exportStudentPDF } from './export-pdf';
-
